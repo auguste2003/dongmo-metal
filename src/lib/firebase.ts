b7c06600419c5c6c09bd3ac64ad361d8a2345430
Auguste Atoundem sonfack <130369166+auguste2003@users.noreply.github.com>
2025-08-29 11:20:09 +0200

    Update firebase.ts
    Add the api key. It is not a problem, when it is public

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "metal-expressions",
   appId: "1:207635817802:web:00153b219cb276d4278db3",
   storageBucket: "metal-expressions.firebasestorage.app",
-  apiKey: "",
+  apiKey: "AIzaSyBMBZuNkoF57EfPNo1DlOmZ1mtiKtd7EQs",
   authDomain: "metal-expressions.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "207635817802"
