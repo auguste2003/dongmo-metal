c5e6d94067c0931dc27e5012ade5b582530aae9f
Auguste sonfack Atoundem <atoundemaugustesonfack@gmail.com>
2025-08-29 09:37:49 +0000

    merge from origin

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,10 +9,7 @@ const firebaseConfig = {
   storageBucket: "metal-expressions.firebasestorage.app",
   apiKey: "AIzaSyBMBZuNkoF57EfPNo1DlOmZ1mtiKtd7EQs",
   authDomain: "metal-expressions.firebaseapp.com",
-  projectId: "metal-expressions",
-  storageBucket: "metal-expressions.appspot.com",
   messagingSenderId: "207635817802",
-  appId: "1:207635817802:web:00153b219cb276d4278db3"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
