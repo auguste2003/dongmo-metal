59c1c06309830454cea246a542a3420f8d4812cd
Auguste sonfack Atoundem <atoundemaugustesonfack@gmail.com>
2025-09-10 14:54:54 +0000

    je vois localhost et je ne vois pas le logo non plus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,8 @@ const ptSans = PT_Sans({
   variable: '--font-pt-sans',
 })
 
-const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
+
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
